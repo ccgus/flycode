dad32d8f89cd8ecda662c6706910b7779aa5f46f
Gus Mueller <gus@flyingmeat.com>
2007-03-21 20:23:30 +0000

    wee

---
+++ buildapp/src/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  BuildApp
+//
+//  Created by August Mueller on 3/21/07.
+//  Copyright Flying Meat Inc. 2007. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
